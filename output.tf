080de45e70f2ab756c5840a298f56ab428cf1df2
Dipu Singh <dipu.devops@gmail.com>
2026-01-16 21:59:14 +0530

    first commit

---
+++ output.tf
@@ -0,0 +1,7 @@
+output "name" {
+    value = "Hello my name is ${var.dsididasidasdas}"
+}
+
+output "username" {
+    value = "User name is ${var.username}"
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+    type = string
+    default = "zahid"
+}
+variable "dsididasidasdas" {
+    type = string
+    default = "zahid"
+}
+variable "username" {
+    type = string
+}
